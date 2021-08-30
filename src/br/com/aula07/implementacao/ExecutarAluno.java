9a986a26fdf8f2ab4010026494db8db8aa5152b8
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-30 19:44:28 -0300

    Criando e instanciando varios tipos de um Vetor

--- src/br/com/aula07/implementacao/ExecutarAluno.java
+++ src/br/com/aula07/implementacao/ExecutarAluno.java
@@ -1,9 +1,36 @@
 package br.com.aula07.implementacao;
 
+import java.util.Scanner;
+
+import br.com.aula07.beans.Aluno;
+
 public class ExecutarAluno {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Aluno[] alunosVetor = new Aluno [3];
+		
+		String nomesVetor[] = {"Beatriz", "Maria", "Ana"};
+		
+		System.out.println(nomesVetor[0]);
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		int[] idadesVetor = new int [3];
+		
+		for (int i=0; i <idadesVetor.length; i++) {
+			idadesVetor[i] = leitor.nextInt();
+		}
+		
+		for (int i=0; i <idadesVetor.length; i++) {
+			System.out.println(idadesVetor[i]);
+		}
+		
+		alunosVetor[0] = new Aluno ("Pedro", 8 , 50, 20);
+		alunosVetor[1] = new Aluno ("Ana", 9 , 5, 21);
+		alunosVetor[2] = new Aluno ("Maria", 10 , 0, 18);
+		
+		System.out.println(alunosVetor[0].getNome());
+		
 
 	}
 
