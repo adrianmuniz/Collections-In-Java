0f6e84920f0f946a94c0cac56e7849c9acaa0fa6
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-30 21:15:52 -0300

    Aprimorando a List

--- src/br/com/aula07/implementacao/ExecutarCollection.java
+++ src/br/com/aula07/implementacao/ExecutarCollection.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import br.com.aula07.beans.Aluno;
 
 public class ExecutarCollection {
@@ -11,7 +13,7 @@ public class ExecutarCollection {
 	public static void main(String[] args) {
 		List<String> nomes = new ArrayList<String>();
 		
-		List<Aluno> alunos = new ArrayList<Aluno>();
+		
 		
 		nomes.add("Beatriz");
 		nomes.add("Pedro");
@@ -22,6 +24,32 @@ public class ExecutarCollection {
 		
 		Collections.sort(nomes);
 		System.out.println("Lista organizada:" + nomes);
+		
+		System.out.println(nomes.get(1));
+		
+		String textoProcura = JOptionPane.showInputDialog("Informe o nome de pesquisa:");
+		if(nomes.indexOf(textoProcura) >= 0) {
+			System.out.println("Pesquisa String "  + nomes.get(nomes.indexOf(textoProcura)));
+		} else {
+			System.out.println("Não localizado");
+		}
+		
+		//Agora com classe
+		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		
+		Aluno a1 = new Aluno ("Pedro", 8 , 50, 20);
+		Aluno a2 = new Aluno ("Ana", 9 , 5, 21);
+		
+		alunos.add(a1);
+		alunos.add(a2);
+		
+		System.out.println(alunos.size());
+		
+		for (Aluno a: alunos) {
+			System.out.println(a.getNome() +  " idade:" + a.getIdade());
+		}
 		
 	}
 
